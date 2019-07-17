231c23863a72b40b8265420c5e61288e8e326aed
Joao <vilarjp@hotmail.com>
2019-07-16 22:46:00 -0300

    Initial commit

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 
-import { Container, Form, SubmitButton } from './styles';
+import Container from '../../components/Container';
+import { Form, SubmitButton, List } from './styles';
 
 export default class Main extends Component {
   state = {
     newRepo: '',
     repositories: [],
+    loading: false,
+    error: false,
   };
+
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
 
   handleInput = event => {
     this.setState({ newRepo: event.target.value });
@@ -18,20 +38,39 @@ export default class Main extends Component {
   handleSubmit = async event => {
     event.preventDefault();
 
-    const { newRepo, repositories } = this.state;
-    const response = await api.get(`/repos/${newRepo}`);
-    const data = {
-      name: response.data.full_name,
-    };
+    this.setState({ loading: true });
 
-    this.setState({
-      repositories: [...repositories, data],
-      newRepo: '',
-    });
+    const { newRepo, repositories } = this.state;
+
+    try {
+      if (repositories.find(repo => repo.name === newRepo)) {
+        const error = {
+          message: 'Duplicated repository',
+        };
+        throw error;
+      }
+
+      const response = await api.get(`/repos/${newRepo}`);
+      const data = {
+        name: response.data.full_name,
+      };
+      this.setState({
+        repositories: [...repositories, data],
+        newRepo: '',
+        loading: false,
+        error: false,
+      });
+    } catch (err) {
+      // eslint-disable-next-line no-alert
+      alert(err.message);
+      this.setState({ error: true });
+    } finally {
+      this.setState({ loading: false });
+    }
   };
 
   render() {
-    const { newRepo } = this.state;
+    const { newRepo, repositories, loading, error } = this.state;
 
     return (
       <Container>
@@ -43,14 +82,30 @@ export default class Main extends Component {
         <Form onSubmit={this.handleSubmit}>
           <input
             type="text"
-            placeholder="Add repository"
+            placeholder="Add repository (ex: facebook/react)"
             value={newRepo}
             onChange={this.handleInput}
+            className={error ? 'error' : ''}
           />
-          <SubmitButton>
-            <FaPlus color="#FFF" size={14} />
+          <SubmitButton loading={loading ? 1 : 0}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
           </SubmitButton>
         </Form>
+
+        <List>
+          {repositories.map(repository => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Details
+              </Link>
+            </li>
+          ))}
+        </List>
       </Container>
     );
   }
--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,149 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-// import { Container } from './styles';
+import api from '../../services/api';
 
-export default function Repository() {
-  return <h1>Repository</h1>;
+import Container from '../../components/Container';
+import { Loading, Owner, IssueList, Filter, Buttons } from './styles';
+
+export default class Repository extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired,
+  };
+
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+    repoName: '',
+    filter: 'all',
+    page: 1,
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repository = decodeURIComponent(match.params.repository);
+    this.setState({ repoName: repository }, () => {
+      const { repoName } = this.state;
+      this.getRepo(repoName);
+    });
+  }
+
+  getRepo = async repoName => {
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    });
+  };
+
+  handleRepos = async (repoName, filter, page) => {
+    const issues = await api.get(`/repos/${repoName}/issues`, {
+      params: {
+        state: filter,
+        per_page: 5,
+        page,
+      },
+    });
+
+    this.setState({
+      issues: issues.data,
+    });
+  };
+
+  setFilter = event => {
+    this.setState({ filter: event.target.value, page: 1 }, () => {
+      const { repoName, filter, page } = this.state;
+      this.handleRepos(repoName, filter, page);
+    });
+  };
+
+  paginate = p => {
+    if (p === 0) return;
+    this.setState({ page: p }, () => {
+      const { repoName, filter, page } = this.state;
+      this.handleRepos(repoName, filter, page);
+    });
+  };
+
+  render() {
+    const { repository, issues, loading, filter } = this.state;
+    const { page } = this.state;
+
+    if (loading) {
+      return <Loading>Loading...</Loading>;
+    }
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Go back to repositories</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+
+        <Filter>
+          <label htmlFor="filter">
+            Filter
+            <select id="filter" value={filter} onChange={this.setFilter}>
+              <option value="all">all</option>
+              <option value="open">open</option>
+              <option value="closed">closed</option>
+            </select>
+          </label>
+        </Filter>
+
+        <IssueList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a
+                    href={issue.html_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {issue.title}
+                  </a>
+                  {issue.labels.map(label => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
+
+        <Buttons>
+          <button
+            type="button"
+            onClick={() => this.paginate(page - 1)}
+            disabled={page === 1}
+          >
+            Previous page
+          </button>
+          <button type="button" onClick={() => this.paginate(page + 1)}>
+            Next page
+          </button>
+        </Buttons>
+      </Container>
+    );
+  }
 }
